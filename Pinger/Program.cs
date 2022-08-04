ec18a1ca5c5782a61fa8234c65385926d29231f9
Julie Tran <kimtran767@gmail.com>
2022-08-04 15:38:12 -0700

    pinger completed with success ip returned

--- Pinger/Program.cs
+++ Pinger/Program.cs
@@ -17,4 +17,13 @@ string address = "4.2.2.2";
 
 PingReply reply = pingSender.Send(address, timeout, buffer, options);
 
+if (reply.Status == IPStatus.Success)
+{
+    Console.WriteLine("Responsse: {0}", reply.Status.ToString());
+    Console.WriteLine("RouteTrip: {0}", reply.RoundtripTime);
+    Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
+    Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
+
+}
+
 
