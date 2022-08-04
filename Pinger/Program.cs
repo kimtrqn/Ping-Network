03459fdf848b262f883ed7c0686bacd16872cfd7
Julie Tran <kimtran767@gmail.com>
2022-08-04 15:11:52 -0700

    Pinger reply and send

--- Pinger/Program.cs
+++ Pinger/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.NetworkInformation;
+using System.Text;
+
+//Pinging Google DNS Server 4.2.2.2;
+
+Ping pingSender = new Ping();
+
+PingOptions options = new PingOptions();
+
+
+options.DontFragment = true;
+
+string data = "Learn to Code";
+byte[] buffer = Encoding.ASCII.GetBytes(data);
+int timeout = 120;
+string address = "4.2.2.2";
+
+PingReply reply = pingSender.Send(address, timeout, buffer, options);
+
 
